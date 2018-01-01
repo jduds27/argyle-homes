a7a676ec7034c71bee064fe1fc05cebc758549c3
Jason Dudley <jason.dudley27@gmail.com>
2018-01-01 12:35:45 -0700

    aded lightbox

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery3
+//= require popper
 //= require rails-ujs
+//= require bootstrap
+//= require lightbox-bootstrap
+//= require jquery_ujs
 //= require turbolinks
+//= require bootstrap-sprockets
 //= require_tree .
