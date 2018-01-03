45704a9c248b2bb461b11e682871961a98785aa3
Jason Dudley <jason.dudley27@gmail.com>
2018-01-03 14:52:05 -0700

    added customer views and route resources

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,18 @@
 class CustomersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def new
+  end
+
+  def delete
+  end
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   
   devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
   resources :photos
+  resources :customers
   
   get 'contact', to: 'pages#contact'
 
